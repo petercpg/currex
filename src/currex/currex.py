fdeb42a3a4517cfdd6d76020d26a706b7c7ef633
Gary Kwong <nth10sd@gmail.com>
2018-03-12 02:31:04 -0700

    Move code out into a separate curr_convert function.

--- src/currex/currex.py
+++ src/currex/currex.py
@@ -9,18 +9,25 @@
 
 from __future__ import division, print_function
 
+import sys
+
 from builtins import input  # pylint: disable=redefined-builtin
+
+
+def curr_convert(curr, rate):
+    inp = input("What is your desired %s amount? (Type \"q\" to quit) " % curr)
+    if inp == "q":
+        sys.exit(0)
+    else:
+        out = "%s%.2f" % ("USD", float(inp) / rate)
+    return out
 
 
 def main():
     inp_curr = input("What is your desired currency? ")
     curr_rate = float(input("What is your desired rate? USD1:%s" % inp_curr))
     while True:
-        inp = input("What is your desired %s amount? (Type \"q\" to quit) " % inp_curr)
-        if inp == "q":
-            break
-        else:
-            print("%s%.2f" % ("USD", float(inp) / curr_rate))
+        print(curr_convert(inp_curr, curr_rate))
 
 
 if __name__ == "__main__":
