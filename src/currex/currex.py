a9b3faed2f55a4c74d6234e219901b7a88baee62
Gary Kwong <nth10sd@gmail.com>
2018-03-12 02:21:52 -0700

    Import print_function from __future__.

--- src/currex/currex.py
+++ src/currex/currex.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from __future__ import division
+from __future__ import division, print_function
 
 from builtins import input
 
