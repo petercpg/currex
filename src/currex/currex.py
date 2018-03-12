b800b91c836e881781e40647d59d2463e5af0915
Gary Kwong <nth10sd@gmail.com>
2018-03-12 01:58:15 -0700

    Add initial iteration of currex.py

---
+++ src/currex/currex.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from __future__ import division
+
+from builtins import input
+
+
+def main():
+    inp_curr = input("What is your desired currency? ")
+    curr_rate = float(input("What is your desired rate? USD1:%s" % inp_curr))
+    while True:
+        inp = input("What is your desired %s amount? (Type \"q\" to quit) " % inp_curr)
+        if inp == "q":
+            break
+        else:
+            print("%s%.2f" % ("USD", float(inp) / curr_rate))
+
+if __name__ == "__main__":
+    main()
