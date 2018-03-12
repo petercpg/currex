0b91fb660f66fd39e22858a912169bd3438e6790
Gary Kwong <nth10sd@gmail.com>
2018-03-12 02:26:30 -0700

    Add module docstring for currex.

--- src/currex/currex.py
+++ src/currex/currex.py
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+"""Simple currency converter.
+"""
+
 from __future__ import division, print_function
 
 from builtins import input
